88f1f959523b2468931c2a5f6b05cfa0034eb0b4
cyberseihis <mcraftdm@gmail.com>
2022-12-28 22:19:57 +0200

    Take number of awards

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -7,7 +7,13 @@ def getStruct (link: str):
     soop = BeautifulSoup(htm)
     full_name = soop.find('div', class_='fn').string
     surname = full_name.split(' ')[-1]
-    return surname
+    awards = soop.find('th', class_='infobox-label', string='Awards')
+    if awards == None:
+        nawards = 0
+    else: 
+        nli = len(awards.next_sibling.find_all('li'))
+        nawards = nli if nli>0 else len(awards.next_sibling.find_all('a'))
+    return surname,nawards
 
 # Get list of urls
 links = pd.read_csv('List of computer scientists - Wikipedia.csv').transpose().index
