d2eaf882d39f4b04f59f9782c843e87304d830d1
cyberseihis <mcraftdm@gmail.com>
2022-12-31 20:10:35 +0200

    fix(kdtree): ties with median now all go left

--- kdtree.hs
+++ kdtree.hs
@@ -5,10 +5,15 @@ data Kd = Kempty | Kx Point Kd Kd | Ky Point Kd Kd deriving (Eq, Show, Ord)
 
 data Point = Point {x :: Int, y :: Int} deriving (Eq, Show, Ord)
 
+-- Maybe using sets would be more convinient?
+-- But how would I take the median
 makeKd _ [] = Kempty
 makeKd dim points = Kx median (subKd less) (subKd more)
   where
-    (less, median : more) = splitInHalf . sortOn (xOry dim) $ points
+    (below, median : above) = splitInHalf . sortOn axis $ points
+    axis = xOry dim
+    thres = axis median
+    (less,more) = break ((>thres).axis) (below++above)
     subKd = makeKd (not dim)
 
 xOry :: Bool -> Point -> Int
