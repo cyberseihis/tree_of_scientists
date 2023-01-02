45ebbe8fbbc1c13a4a5c0d44243eabb74b187c1c
cyberseihis <mcraftdm@gmail.com>
2023-01-02 23:01:25 +0200

    Test for complete int range returing everything

--- KdTest.hs
+++ KdTest.hs
@@ -1,6 +1,7 @@
 module KdTest where
 import Kdtree
 import Test.QuickCheck
+import Data.List ((\\))
 
 instance Arbitrary Point where
     arbitrary = do
@@ -12,13 +13,19 @@ instance Arbitrary Kd where
         makeKd <$> arbitrary
 
 prop_all_in pl ph kdt = smaller pl ph ==>
-    all (isCovered pl ph) . map normaliseToX $ rangeKd pl ph kdt
+    all (isCovered pl ph . normaliseToX) $ rangeKd pl ph kdt
+
+prop_all_minmax xs =
+    let minPoint = Pointx minBound minBound
+        maxPoint = Pointx maxBound maxBound
+        kdt = makeKd xs
+        ys = map normaliseToX $ rangeKd minPoint maxPoint kdt
+    in samesame xs ys
+
+samesame xs ys = null (xs \\ ys) && null (ys \\ xs)
 
 smaller :: Point -> Point -> Bool
 smaller pl ph = pl <= ph && other pl <= other ph
 
 normaliseToX (Pointy x y) = Pointx x y
 normaliseToX p = p
-
--- >>> quickCheck prop_all_in
-
