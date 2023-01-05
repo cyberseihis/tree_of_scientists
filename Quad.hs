9d4ac6774bf4088c1063b56c6d7cf43474fee283
cyberseihis <mcraftdm@gmail.com>
2023-01-05 14:23:48 +0200

    Start of splitting function

--- Quad.hs
+++ Quad.hs
@@ -3,14 +3,35 @@ import Kdtree
 import Data.List.NonEmpty (groupAllWith, toList)
 import Data.Map.Strict ( Map )
 import Control.Arrow (Arrow((&&&), first), (>>>))
+import qualified Data.Map as Map
+import qualified Data.List as List
 
 data Qtree = Qempty | Qtree Point (Map Quad Qtree) deriving (Eq,Show)
 type Quad = (Bool,Bool)
 
 compQuarts x y = (x <= y, other x <= other y)
 
-splitQuarts x ys =
-    let z = map (compQuarts x &&& id) ys
-        ff = groupAllWith fst z
-        hh = map (toList >>> unzip >>> first head) ff
+emptyQuart = Map.fromList . zip [(x,y)|x<-bls,y<-bls] $ repeat []
+    where bls = [False,True]
+
+splitAround x =
+    map (compQuarts x &&& id) >>>
+    groupAllWith fst >>>
+    map (toList >>> unzip >>> first head) >>>
+    Map.fromList >>>
+    (`Map.union` emptyQuart)
+
+spreadAround x =
+    splitAround x >>>
+    Map.elems >>>
+    map length >>>
+    (maximum &&& minimum) >>>
+    uncurry (-)
+
+makeQtree :: [Point] -> Qtree
+makeQtree points =
+    let posibleSpilts = removeEach points
+        
     in undefined
+
+removeEach xs = zip xs (map (`List.delete` xs) xs)
