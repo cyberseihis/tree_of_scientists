3f0e1753cfda12a3bb4d15881504a445b7ecfd08
cyberseihis <mcraftdm@gmail.com>
2023-01-08 00:19:11 +0200

    Rangetree ordinality instance

--- RangeTree.hs
+++ RangeTree.hs
@@ -1,28 +1,31 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE InstanceSigs #-}
 module RangeTree where
 import Kdtree
 import Control.Arrow ((>>>))
 import Data.List (sort, group, partition, singleton)
 
 data OneTree a =
-    Onempty | Oleaf (Bunch a) |
+    Onempty | Oleaf a |
     Onode {
-        n :: Bunch a,
+        n :: a,
         less :: OneTree a,
         more :: OneTree a} deriving (Eq,Show)
 
-newtype Bunch a = Bunch [a] deriving (Eq,Show)
+getOut (Oleaf x) = x
+getOut Onode {n=x} = x
 
-instance Ord a => Ord (Bunch a) where
-    (<=) :: Ord a => Bunch a -> Bunch a -> Bool
-    Bunch (x:_) <= Bunch (y:_) = x <= y
+instance Ord a => Ord (OneTree a) where
+    compare Onempty Onempty = EQ
+    compare Onempty _ = LT
+    compare _ Onempty = GT
+    compare x y = compare (getOut x) (getOut y)
+
+newtype Bunch a = Bunch [a] deriving (Eq,Show,Ord)
 
 matchUp :: Ord a => [a] -> [Bunch a]
 matchUp = sort >>> group >>> map Bunch
 
-makeRang :: Ord a => [a] -> OneTree a
 makeRang = flip makeOne [] . matchUp
 
 rangeRang low high ontree = concatMap (\(Bunch a)->a) $
@@ -33,9 +36,12 @@ rangeRang low high ontree = concatMap (\(Bunch a)->a) $
 makeOne [] [] = Onempty
 makeOne [x] [] = Oleaf x
 makeOne [] [x] = Oleaf x
-makeOne xs ys = Onode median (makeOne less (median:ls)) (makeOne more mr) where
+makeOne xs ys =
+    Onode median mkLess mkMore where
     (less, median, more) = splitApart xs
     (ls,mr) = partition (<=median) ys
+    mkLess = makeOne less (median:ls)
+    mkMore = makeOne more mr
 
 rangeOne _ _ Onempty = []
 rangeOne low high (Oleaf n) = [n | low <=n && n<=high]
@@ -63,9 +69,6 @@ report Onode {..} = concatMap report [less,more]
 
 isSplitnode low high Onode {n} = low <= n && n <= high
 
-isSplitnodeBad low high o@(Onode {..}) = all (inrange low high) [o,less,more]
-    where
-    inrange _ _ Onempty = False
-    inrange low high x = let v = grabVal x in low <= v && v <= high
-    grabVal (Oleaf x) = x
-    grabVal Onode {n} = n
+tstt = rangeRang 40 70 xx where
+    b = [3,8..100]++[63,68,43,68]
+    xx = makeRang b
