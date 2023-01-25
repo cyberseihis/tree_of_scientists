86385822df3bb3e92a9a7f26ca0efd08593f95d1
cyberseihis <mcraftdm@gmail.com>
2023-01-25 20:32:05 +0200

    Helper function for lsh data exploration idk

--- EzHash.hs
+++ EzHash.hs
@@ -4,6 +4,7 @@ import LshMinhash
 import Data.List (group, sort)
 import Control.Arrow ((&&&))
 import Data.Functor ((<&>))
+import Data.List.Split (chunksOf)
 
 educate = unwords . drop 2 . words
 stories = map educate . lines
@@ -29,10 +30,32 @@ getAnIdea2 xs =
 secondOpinion xs =
     [sigKinda x y|x<-xs,y<-xs,x/=y]
 
+checkBins :: [Int] -> [Int] -> Int -> Int -> Bool
+checkBins minh1 minh2 r b =
+    let swag1 = take b . chunksOf r $ minh1
+        swag2 = take b . chunksOf r $ minh2
+    in or $ zipWith (==) swag1 swag2
+
+reconsideration xs r b =
+    [checkBins x y r b|x<-xs,y<-xs,x/=y]
+
+reportChecks r b t = do
+    minhas <- readFile "myHashes.csv"
+    let mhas = read minhas :: [[Int]]
+        reon = reconsideration mhas r b
+    sx <- readFile "jaccardsFull.csv"
+    let jac = read sx :: [Double]
+        stuc = map (>t) jac
+        bro = map (head &&& length) .
+            group . sort $ zip reon stuc
+    print bro
+
+
+
 sortData = readFile "sorteddata.csv"
 -- Stats of similarities
 -- [(.0,1153),(.1,2784),(.2,672),(.3,44),(.4,3)]
 -- max similariry is .44
 -- [((False,False),3345),((False,True),49),((True,False),658),((True,True),604)] for signares O.2
 main =
-    sortData >>= print . minHashTest . lines
+    sortData >>= writeFile "jaccardsFull.csv". show . getAnIdea2 . lines
