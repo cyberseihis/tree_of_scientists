bf77e857dc9ed6f4a680f8880c5730f657ab902b
cyberseihis <mcraftdm@gmail.com>
2023-01-27 22:00:00 +0200

    Script that gets profiled done

--- Profil.hs
+++ Profil.hs
@@ -2,15 +2,17 @@ module Profil where
 
 import Dumped (ktree,qtree,twotree,rtree)
 import Commander (execution,Query)
-import Test.QuickCheck (quickCheck)
+import Test.QuickCheck (quickCheck, withMaxSuccess, Args (maxSuccess, chatty), quickCheckWith, stdArgs)
 import KdTest (GeneralTree)
 
 sampleQuery :: GeneralTree a => a -> Query -> Bool
 sampleQuery tree query = execution tree query `seq` True
 
-krun = quickCheck $ sampleQuery ktree
-qrun = quickCheck $ sampleQuery qtree
-trun = quickCheck $ sampleQuery twotree
-rrun = quickCheck $ sampleQuery rtree
+heatCPU tree = quickCheckWith stdArgs {maxSuccess=100000,chatty=False} $ sampleQuery tree
+
+krun = heatCPU ktree
+qrun = heatCPU qtree
+trun = heatCPU twotree
+rrun = heatCPU rtree
 
 go = sequence_ [krun,qrun,trun,rrun]
