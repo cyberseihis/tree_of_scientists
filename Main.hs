ffdd8a65ef7914ffbd88e55e7983cae9df0f0043
cyberseihis <mcraftdm@gmail.com>
2023-01-27 20:57:18 +0200

    Demo script ready

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Commander
-import Dumped (ktree,theBands,wordSets)
+import Dumped (ktree,theBands,wordSets,prettys)
 import Data.List (sort)
 import LshMinhash (sanityJacc)
 import Control.Arrow (Arrow((***)))
@@ -9,9 +9,14 @@ import Control.Arrow (Arrow((***)))
 jac = uncurry sanityJacc . (ws *** ws) where
     ws = (wordSets!!)
 
+prettyPrint (i,j) = do
+    putStrLn $ prettys!!i
+    putStrLn $ prettys!!j
+    putStrLn ""
+
 main = do
-    let query = Query 'H' 'R' 2 8
+    [[ls],[lm],ss,sm] <- words <$> getLine
+    let query = Query ls lm (read ss) (read sm)
         exe = execution ktree query
-    print . filter jac $ candidates theBands exe
-    print . filter jac $ everyPair exe
-    print $ candidates theBands exe
+        couples = filter jac $ candidates theBands exe
+    mapM_ prettyPrint couples
