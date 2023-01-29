197d0dc668e067de686683aa608858b0ac0b1907
cyberseihis <mcraftdm@gmail.com>
2023-01-30 00:53:39 +0200

    Unnesesary refactoring in commander

--- Commander.hs
+++ Commander.hs
@@ -8,7 +8,7 @@ import Quad
 import RTree
 import RangeTree
 import Data.Char (ord)
-import Control.Arrow ((>>>), Arrow ((&&&), (***)))
+import Control.Arrow ((>>>), Arrow ((&&&), (***), second))
 import KdTest (GeneralTree(make, querry))
 import Test.QuickCheck (Arbitrary (arbitrary), choose)
 import Data.List (sort)
@@ -32,11 +32,13 @@ instance Arbitrary Query where
         pb <- choose (ps,13)
         return $ Query ls lb ps pb
 
+queryPoints :: Query -> (Point,Point)
+queryPoints Query {..} =
+    (Pointx (alphaOrd letterMin) prizesMin 0, Pointx (alphaOrd letterMax) prizesMax 0)
+
 execution :: GeneralTree a => a -> Query -> [Index]
-execution tree Query {..} =
-    let low = Pointx (alphaOrd letterMin) prizesMin 0
-        high = Pointx (alphaOrd letterMax) prizesMax 0
-    in stuff <$> querry low high tree
+execution = curry $
+    map stuff . (uncurry.flip.uncurry) querry.second queryPoints
 
 candidates :: [[Band]] -> [Index] -> [IPair]
 candidates bmatrix indices =
