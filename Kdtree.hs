5cfcd85a62691c79999f0c76593008c7eb6cadd2
cyberseihis <mcraftdm@gmail.com>
2023-02-02 15:58:52 +0200

    Kd also uses mean now

--- Kdtree.hs
+++ Kdtree.hs
@@ -1,32 +1,33 @@
 {-# LANGUAGE LexicalNegation #-}
 module Kdtree  where
-import Data.List (sortOn, sort)
+import Data.List (sortOn, sort, partition)
 import Control.Arrow (Arrow((&&&)), (>>>))
 
-data Kd = Kempty | Kd Point Kd Kd deriving (Eq, Show)
+data Kd = Kempty | Kleaf Point | Kd Point Kd Kd deriving (Eq, Show)
 
 data Point = Pointx {x :: Double, y :: Double, stuff :: [Int]} | Pointy {x :: Double, y :: Double, stuff :: [Int]} deriving (Eq, Show)
 
 instance Ord Point where
-    (Pointx x _ _) `compare` (Pointx z _ _) = x `compare` z
-    (Pointy _ y _) `compare` (Pointy _ z _) = y `compare` z
+    (Pointx x _ _) <= (Pointx z _ _) = x <= z
+    (Pointy _ y _) <= (Pointy _ z _) = y <= z
 
 other (Pointx x y i) = Pointy x y i
 other (Pointy x y i) = Pointx x y i
 
 makeKd [] = Kempty
-makeKd points = Kd median (subKd less) (subKd more)
-  where (less, median, more) = splitApart points
+makeKd [x] = Kleaf x
+makeKd points = Kd mid (subKd less) (subKd more)
+  where mid = meanp points
+        (less,more) = partition (<=mid) points
         subKd = makeKd . map other
 
 rangeKd :: Point -> Point -> Kd -> [Point]
 rangeKd _ _ Kempty = []
-rangeKd lowp highp (Kd point kd1 kd2) =
-    let nextQuerry = rangeKd (other lowp) (other highp)
-        above = if point <= highp then nextQuerry kd2 else []
-        below = if lowp <= point then nextQuerry kd1 else []
-        mid = [point | isCovered lowp highp point]
-    in concat [below, mid, above]
+rangeKd low high (Kleaf x) = [x | isCovered low high x]
+rangeKd low high (Kd point kd1 kd2) =
+    let nextQuerry = rangeKd (other low) (other high)
+        h = [kd2|point <= high] ++ [kd1|low <= point]
+    in concatMap nextQuerry h
 
 isCovered low high point =
     low <= point && point <= high &&
@@ -49,3 +50,12 @@ splitEarly xs =
     (below, median : above) = splitDownHalf . sort $ xs
     (less,more) = break (>median) (below++above)
     in (less,median,more)
+
+meanp :: [Point] -> Point
+meanp xs =
+    let n = fromIntegral $ length xs
+        mx = (/n).sum.map x $ xs
+        my = (/n).sum.map y $ xs
+        pt Pointx {} = Pointx
+        pt Pointy {} = Pointy
+    in pt (head xs) mx my []
--- Quad.hs
+++ Quad.hs
@@ -9,11 +9,14 @@ import qualified Data.List as List
 import Data.Ord (comparing)
 import Data.Foldable (maximumBy)
 import Data.List (nub, sortOn, sort)
+import Data.Maybe (mapMaybe)
 
 data Qtree =
     Qempty | Qleaf Point |
     Qtree {m::Point,ne::Qtree,se::Qtree,sw::Qtree,nw::Qtree}
     deriving (Eq,Show)
+
+fielt = [((False,False),ne),((False,True),nw),((True,False),se),((True,True),sw)]
 
 compQuarts x y = (x <= y, other x <= other y)
 
@@ -27,15 +30,12 @@ makeQtree points =
         [ne,nw,se,sw] = g <$> bls
     in Qtree {m=m, ne=ne, se=se, sw=sw, nw=nw}
 
-fielt = Map.fromList
-    [((False,False),ne),((False,True),nw),((True,False),se),((True,True),sw)]
-
 rangeQt :: Point -> Point -> Qtree -> [Point]
 rangeQt _ _ Qempty = []
 rangeQt low high (Qleaf x) = [x | isCovered low high x]
 rangeQt low high qt@(Qtree {m}) =
     let [(j,i),(h,k)] = compQuarts m <$> [low,high]
-        hj = map (fielt!) . nub $ [(j,i),(h,i),(j,k),(h,k)]
+        hj = mapMaybe (`lookup` fielt) . nub $ [(j,i),(h,i),(j,k),(h,k)]
     in concatMap (rangeQt low high) $ hj <*> [qt]
 
 medX :: [Point] -> Point
@@ -43,11 +43,4 @@ medX xs =
     let n = length xs `div` 2
         mx = (!!n).sort.map x $ xs
         my = (!!n).sort.map y $ xs
-    in Pointx mx my []
-
-meanp :: [Point] -> Point
-meanp xs =
-    let n = fromIntegral $ length xs
-        mx = (/n).sum.map x $ xs
-        my = (/n).sum.map y $ xs
     in Pointx mx my []
