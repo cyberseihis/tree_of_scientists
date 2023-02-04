8259790591123d398e40a4de98eb24b9fb9d9ca2
cyberseihis <mcraftdm@gmail.com>
2023-02-04 20:01:25 +0200

    Some refactoring in rangetree

--- RangeTree.hs
+++ RangeTree.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
-module RangeTree where
+module RangeTree (TwoTree (..),makeRange,rangeRange) where
 import Kdtree
 import Control.Arrow ((>>>))
 import Data.List (sort, group, partition, singleton, groupBy, sortOn)
@@ -13,7 +13,7 @@ data OneTree =
         more :: OneTree} deriving (Eq,Show)
 
 data TwoTree =
-    Tnempty | Tleaf { tval :: Bunch, tlide :: OneTree} |
+    Tnempty | Tleaf { tn :: Bunch, tside :: OneTree} |
     Tnode {
         tn :: Bunch,
         tside :: OneTree,
@@ -30,7 +30,7 @@ instance Ord Bunch where
     Bunch (x:_) <= Bunch (y:_) = x <= y
 
 makeRange :: [Point] -> TwoTree
-makeRange = matchUp >>> makeTwo
+makeRange = matchUp x >>> makeTwo
 
 rangeRange :: Point -> Point -> TwoTree -> [Point]
 rangeRange low high tree =
@@ -43,8 +43,7 @@ rangeRange low high tree =
 bother :: Bunch -> Bunch
 bother (Bunch x) = Bunch . map other $ x
 
-matchUp = sortOn x >>> groupBy (\pa pb->x pa == x pb) >>> map Bunch
-matchUpy = sortOn y >>> groupBy (\pa pb->y pa == y pb) >>> map Bunch
+matchUp x = sortOn x >>> groupBy (\pa pb->x pa == x pb) >>> map Bunch
 
 makeOne [] = Onempty
 makeOne [x] = Oleaf x
@@ -64,16 +63,16 @@ makeTwo xs =
     mkMore = makeTwo more
 
 makeSide :: [Bunch] -> OneTree
-makeSide = makeOne . matchUpy . map other . concatMap unBunch
+makeSide = makeOne . matchUp y . map other . concatMap unBunch
 
 rangeTwo _ _ Tnempty = []
-rangeTwo low high (Tleaf bun sid) =
-    if (low <= bun) && (high >= bun)
+rangeTwo low high tl@(Tleaf bun sid) =
+    if isTSplitnode low high tl
     then rangeSide low high sid
     else []
 rangeTwo low high o@(Tnode {..})
     | isTSplitnode low high o =
-        twoRangeLeft low high tless ++ twoRangeRight low high tmore
+        secondRange low high False tless ++ secondRange low high True tmore
     | low <= tn = rangeTwo low high tless
     | otherwise = rangeTwo low high tmore
 
@@ -83,52 +82,23 @@ rangeOne _ _ Onempty = []
 rangeOne low high (Oleaf n) = [n|(low <= n) && (high >= n)]
 rangeOne low high o@(Onode {..})
     | isSplitnode low high o =
-        rangeLeft low high less ++ rangeRight low high more
+        rangeOne low high less ++ rangeOne low high more
     | low <= n = rangeOne low high less
     | otherwise = rangeOne low high more
 
--- You can remove high from parameters
-rangeLeft _ _ Onempty = []
-rangeLeft low high (Oleaf n) = [n | low <= n]
-rangeLeft low high Onode {..}
-    | low > n= rangeLeft low high more
-    | otherwise = report more ++ rangeLeft low high less
-
-rangeRight _ _ Onempty = []
-rangeRight low high (Oleaf n) = [n |high >= n]
-rangeRight low high Onode {..}
-    | high <= n  = rangeRight low high less
-    | otherwise = report less ++ rangeRight low high more
-
-twoRangeLeft :: Bunch -> Bunch -> TwoTree -> [Bunch]
-twoRangeLeft _ _ Tnempty = []
-twoRangeLeft low high (Tleaf bun sid) =
-    if low <= bun
-    then rangeSide low high sid
-    else []
-twoRangeLeft low high Tnode {..}
-    | low > tn= twoRangeLeft low high tmore
-    | otherwise = reportTwo low high tmore ++ twoRangeLeft low high tless
-
-twoRangeRight :: Bunch -> Bunch -> TwoTree -> [Bunch]
-twoRangeRight _ _ Tnempty = []
-twoRangeRight low high (Tleaf bun sid) =
-    if high >= bun
-    then rangeSide low high sid
-    else []
-twoRangeRight low high Tnode {..}
-    | high <= tn = twoRangeRight low high tless
-    | otherwise = reportTwo low high tless ++ twoRangeRight low high tmore
+secondRange :: Bunch -> Bunch -> Bool -> TwoTree -> [Bunch]
+secondRange _ _ _ Tnempty = []
+secondRange low high _ tleaf@(Tleaf _ _) = rangeTwo low high tleaf
+secondRange low high lor Tnode {..}
+    | high <= tn = secondRange low high lor tless
+    | low > tn= secondRange low high lor tmore
+    | lor = reportTwo low high tless ++ secondRange low high lor tmore
+    | otherwise = reportTwo low high tmore ++ secondRange low high lor tless
 
 dropDim Tnempty = Onempty
-dropDim (Tleaf _ sid) = sid
-dropDim Tnode {tside} = tside
+dropDim x = tside x
 
 reportTwo low high = rangeSide low high . dropDim
 
-report Onempty = []
-report (Oleaf n) = [n]
-report Onode {..} = concatMap report [less,more]
-
 isSplitnode low high Onode {n} = (low <= n) && (high >= n)
-isTSplitnode low high Tnode {tn} = (low <= tn) && (high >= tn)
+isTSplitnode low high node = (low <= tn node) && (high >= tn node)
