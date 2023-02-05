283e1146d8b520fc6e672f959c22d6768038b717
cyberseihis <mcraftdm@gmail.com>
2023-02-05 17:21:31 +0200

    Benchmarks complete, kdtree won

--- Profil.hs
+++ Profil.hs
@@ -4,18 +4,25 @@ import Test.QuickCheck (quickCheck, withMaxSuccess, Args (maxSuccess, chatty), q
 import KdTest (GeneralTree (make, querry))
 import Control.Monad (replicateM)
 import Kdtree
-import Criterion.Main (defaultMain, bench, nfIO)
+import Criterion.Main (defaultMain, bench, nfIO, bgroup)
 import Control.DeepSeq (deepseq)
+import Quad (Qtree(Qtree))
+import RangeTree (TwoTree)
+import RTree (RTree)
 
 sampleQuery :: GeneralTree a => a -> Point -> Point -> Bool
 sampleQuery tree p1 p2 = map stuff (querry p1 p2 tree) `deepseq` True
 
-heatCPU tree = quickCheckWith stdArgs {maxSuccess=10000,chatty=False} $ sampleQuery tree
+heatCPU tree = quickCheckWith stdArgs {chatty=False} $ sampleQuery tree
 
 main = do
     points <- replicateM 100 (generate arbitrary):: IO [Point]
-    let
-        ktree :: Kd = make points
-    defaultMain [
-        bench "trees" $ nfIO (heatCPU ktree)
-        ]
+    let ktree :: Kd = make points
+        qtree :: Qtree = make points
+        ratree :: TwoTree = make points
+        rtree :: RTree = make points
+    defaultMain [ bgroup "trees"
+        [bench "Kd" $ nfIO (heatCPU ktree)
+        ,bench "Quad" $ nfIO (heatCPU ktree)
+        ,bench "Range" $ nfIO (heatCPU ktree)
+        ,bench "R" $ nfIO (heatCPU ktree)]]
