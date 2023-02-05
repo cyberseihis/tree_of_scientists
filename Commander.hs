288734f40ed92543a1162a667052e797c44a21ea
cyberseihis <mcraftdm@gmail.com>
2023-02-05 18:45:21 +0200

    First draft of new executable

--- Commander.hs
+++ Commander.hs
@@ -4,9 +4,6 @@
 module Commander where
 
 import Kdtree
-import Quad
-import RTree
-import RangeTree
 import Data.Char (ord)
 import Control.Arrow ((>>>), Arrow ((&&&), (***), second))
 import KdTest (GeneralTree(make, querry))
@@ -14,31 +11,23 @@ import Test.QuickCheck (Arbitrary (arbitrary), choose)
 import Data.List (sort)
 import LshMinhash
 
-alphaOrd = (- ord 'A') . ord
+alphaOrd = fromIntegral . (- ord 'A') . ord
 
 data Query = Query
     {letterMin :: Char
     ,letterMax :: Char
-    ,prizesMin :: Int
-    ,prizesMax :: Int} deriving (Eq,Show)
+    ,prizesMin :: Double
+    ,prizesMax :: Double} deriving (Eq,Show)
 
 type IPair = (Index,Index)
 
-instance Arbitrary Query where
-    arbitrary = do
-        ls <- choose ('A','Z')
-        lb <- choose (ls,'Z')
-        ps <- choose (0,13)
-        pb <- choose (ps,13)
-        return $ Query ls lb ps pb
-
 queryPoints :: Query -> (Point,Point)
 queryPoints Query {..} =
-    (Pointx (alphaOrd letterMin) prizesMin 0, Pointx (alphaOrd letterMax) prizesMax 0)
+    (Pointx (alphaOrd letterMin) prizesMin [], Pointx (alphaOrd letterMax) prizesMax [])
 
 execution :: GeneralTree a => a -> Query -> [Index]
 execution = curry $
-    map stuff . (uncurry.flip.uncurry) querry.second queryPoints
+    concatMap stuff . (uncurry.flip.uncurry) querry.second queryPoints
 
 candidates :: [[Band]] -> [Index] -> [IPair]
 candidates bmatrix indices =
--- Main.hs
+++ Main.hs
@@ -1,12 +1,18 @@
 module Main where
 import Commander
-import Dumped (rtree,theBands,wordSets,prettys)
-import Data.List (sort)
+import Dumped (theBands,wordSets,prettys)
+import Kdtree
+import KdTest
+import Data.List (sort, singleton)
 import LshMinhash (sanityJacc)
 import Control.Arrow (Arrow((***)))
 
+poinify = (\[c:_,i]->Pointx (alphaOrd c) (read i).singleton).words
+ktree = make points :: Kd
+points = zipWith poinify prettys [0..]
+
 main = getLine >>= mapM_ prettyPrint .filter jac . candidates theBands.
-    execution rtree . parse.words
+    execution ktree . parse.words
     where
     jac = uncurry sanityJacc . (ws *** ws)
     ws = (wordSets!!)
