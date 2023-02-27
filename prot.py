6f086174a2af0259fc34b8ba80c96f1edcf2fe97
cyberseihis <mcraftdm@gmail.com>
2023-02-28 01:24:01 +0200

    Plots for the 4 trees

--- prot.py
+++ prot.py
@@ -8,11 +8,11 @@ def plot_points_and_rectangles(points, rectangles):
     # plot points
     x_points = [p[0] for p in points]
     y_points = [p[1] for p in points]
-    ax.plot(x_points, y_points, 'ro')
+    ax.plot(x_points, y_points, 'bo')
 
     # plot rectangles
     for r in rectangles:
-        rect = patches.Rectangle((r[0], r[1]), r[2]-r[0], r[3]-r[1], linewidth=1, edgecolor='r', facecolor='none')
+        rect = patches.Rectangle((r[0], r[1]), r[2]-r[0], r[3]-r[1], linewidth=1, edgecolor='k', facecolor='none')
         ax.add_patch(rect)
 
     plt.show()
